6e4b4a7082b23ee5d8fa3730b0f24513724abc7f
Baseem ali ch <baseemch43@gmail.com>
2025-04-07 23:21:21 +0530

    db changed

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -4,14 +4,7 @@ dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    // Add these options to bypass SRV records
-    const options = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      directConnection: true
-    };
-    
-    await mongoose.connect(process.env.MONGO_URI as string, options);
+    await mongoose.connect(process.env.MONGO_URI as string);
     console.log("mongoDB connected successfully");
   } catch (error) {
     console.log("Failed to connect mongoDB", error);
