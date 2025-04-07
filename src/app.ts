ccdee8e5a631f3f3b8a9fe01fb543dc0d78df1d5
Baseem ali ch <baseemch43@gmail.com>
2025-04-07 19:16:34 +0530

    make cors globaly

--- src/app.ts
+++ src/app.ts
@@ -34,7 +34,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(
   cors({
-    origin:['https://eduloom.fun','https://www.eduloom.fun'],
+    origin:'*',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
