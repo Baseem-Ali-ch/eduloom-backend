b50d529028e76430dfbc6a7d3e5af108b3313a1d
Baseem ali ch <baseemch43@gmail.com>
2025-04-07 19:32:30 +0530

    change origin t true

--- src/app.ts
+++ src/app.ts
@@ -34,9 +34,10 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(
   cors({
-    origin:'*',
+    origin: true,
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+
     allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
@@ -51,7 +52,7 @@ app.use('/shared', sharedRouter);
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['https://eduloom.fun','https://www.eduloom.fun'],
+    origin: ['https://eduloom.fun', 'https://www.eduloom.fun'],
     credentials: true,
   },
 });
