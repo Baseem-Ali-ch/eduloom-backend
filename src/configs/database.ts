6c9f68ca07bfcd8f221e8c354af17816ac96ac26
Baseem ali ch <baseemch43@gmail.com>
2025-04-07 21:06:24 +0530

    increase timeout in db

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,13 +1,17 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const connectDB = async (): Promise<void> => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log("mongoDB connected successfully");
+    await mongoose.connect(process.env.MONGO_URI as string, {
+      serverSelectionTimeoutMS: 30000,
+      socketTimeoutMS: 45000,
+      retryWrites: true,
+    });
+    console.log('mongoDB connected successfully');
   } catch (error) {
-    console.log("Failed to connect mongoDB", error);
+    console.log('Failed to connect mongoDB', error);
   }
 };
 
